07ee4e32d48871588b720d1670b31d65d57975f4
Julian Cardona <julian@kogimobile.com>
2017-09-05 13:33:28 -0500

    [feat]: add base sample and base library project
    Resolves: #
    See also: #

---
+++ sample/src/main/java/com/pixelatedleaft/kore/sample/ActivityHome.java
@@ -0,0 +1,39 @@
+package com.pixelatedleaft.kore.sample;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.pixelatedleaft.kore.sample.databinding.ActivityHomeBinding;
+
+public class ActivityHome extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    private ActivityHomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_home);
+        setUpNavBar();
+    }
+
+    private void setUpNavBar(){
+        binding.navBar.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                return true;
+            case R.id.navigation_dashboard:
+                return true;
+            case R.id.navigation_notifications:
+                return true;
+        }
+        return false;
+    }
+}
